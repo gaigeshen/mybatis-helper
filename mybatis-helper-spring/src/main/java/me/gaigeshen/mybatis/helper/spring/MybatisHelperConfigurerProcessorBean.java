9fcc91fdf51d2df80170f31510835a48c0121c87
gaigeshen <gaigeshen@qq.com>
2019-07-02 11:00:47 +0800

    Add comments

--- mybatis-helper-spring/src/main/java/me/gaigeshen/mybatis/helper/spring/MybatisHelperConfigurerProcessorBean.java
+++ mybatis-helper-spring/src/main/java/me/gaigeshen/mybatis/helper/spring/MybatisHelperConfigurerProcessorBean.java
@@ -2,6 +2,8 @@ package me.gaigeshen.mybatis.helper.spring;
 
 import me.gaigeshen.mybatis.helper.MybatisHelperConfigurer;
 import org.mybatis.spring.mapper.MapperFactoryBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.BeanPostProcessor;
@@ -14,14 +16,19 @@ import org.springframework.context.ApplicationContextException;
  * @author gaigeshen
  */
 public class MybatisHelperConfigurerProcessorBean implements BeanFactoryPostProcessor, BeanPostProcessor {
+
+  private static final Logger log = LoggerFactory.getLogger(MybatisHelperConfigurerProcessorBean.class);
+
   @Override
   public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+    log.info("Configure mybatis helper");
     MybatisHelperConfigurer.create().configure();
   }
 
   @Override
   public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
     if (bean instanceof MapperFactoryBean) {
+      log.debug("Initialize result mappings of MapperFactoryBean: {}", beanName);
       try {
         MybatisHelperConfigurer.create().initializeResultMappings(
                 ((MapperFactoryBean<?>) bean).getSqlSessionFactory().getConfiguration());
--- mybatis-helper-spring/src/main/java/me/gaigeshen/mybatis/helper/spring/MybatisHelperSqlSessionFactoryBean.java
+++ mybatis-helper-spring/src/main/java/me/gaigeshen/mybatis/helper/spring/MybatisHelperSqlSessionFactoryBean.java
@@ -3,6 +3,8 @@ package me.gaigeshen.mybatis.helper.spring;
 import me.gaigeshen.mybatis.helper.MybatisHelperConfigurer;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.mapper.MapperFactoryBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContextException;
@@ -16,11 +18,14 @@ import org.springframework.context.ApplicationContextException;
  */
 public class MybatisHelperSqlSessionFactoryBean extends SqlSessionFactoryBean implements BeanPostProcessor {
 
+  private static final Logger log = LoggerFactory.getLogger(MybatisHelperSqlSessionFactoryBean.class);
+
   /**
    * Create {@link MybatisHelperSqlSessionFactoryBean}
    */
   public MybatisHelperSqlSessionFactoryBean() {
     // Configure when create this object
+    log.info("Configure mybatis helper");
     MybatisHelperConfigurer.create().configure();
   }
 
@@ -29,6 +34,7 @@ public class MybatisHelperSqlSessionFactoryBean extends SqlSessionFactoryBean im
     // We invoke initialize result mappings method
     // after every mapper factory bean initialization
     if (bean instanceof MapperFactoryBean) {
+      log.debug("Initialize result mappings of MapperFactoryBean: {}", beanName);
       try {
         MybatisHelperConfigurer.create().initializeResultMappings(
                 ((MapperFactoryBean<?>) bean).getSqlSessionFactory().getConfiguration());
