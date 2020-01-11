47888bd4053fb3c2d854a7428cbbcaec3078dd82
gaigeshen <gaigeshen@qq.com>
2020-01-11 12:11:40 +0800

    Define BaseAuditEntity class

---
+++ mybatis-helper/src/main/java/me/gaigeshen/mybatis/helper/BaseAuditEntity.java
@@ -0,0 +1,31 @@
+package me.gaigeshen.mybatis.helper;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Base entity with create time and update time field
+ *
+ * @author gaigeshen
+ */
+public abstract class BaseAuditEntity<ID extends Serializable>
+        extends BaseEntity<ID> {
+  private Date createTime;
+  private Date updateTime;
+
+  public Date getCreateTime() {
+    return createTime;
+  }
+
+  public void setCreateTime(Date createTime) {
+    this.createTime = createTime;
+  }
+
+  public Date getUpdateTime() {
+    return updateTime;
+  }
+
+  public void setUpdateTime(Date updateTime) {
+    this.updateTime = updateTime;
+  }
+}
